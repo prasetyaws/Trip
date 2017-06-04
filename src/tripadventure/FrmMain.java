98f362de26f3f540467446621c28301ab5b31297
prasetyaws <thekingfps@gmail.com>
2017-06-04 21:19:46 +0700

    akhirnya

---
+++ src/tripadventure/FrmMain.java
@@ -0,0 +1,259 @@
+package tripadventure;
+
+public class FrmMain extends javax.swing.JFrame {
+
+    public FrmMain() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        pnl4 = new javax.swing.JPanel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        pnl3 = new javax.swing.JPanel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        pnl2 = new javax.swing.JPanel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        pnl1 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+        });
+        getContentPane().setLayout(null);
+
+        pnl4.setBackground(new java.awt.Color(25, 118, 210));
+        pnl4.setLayout(null);
+
+        jLabel8.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel8.setText("Halaman 4");
+        pnl4.add(jLabel8);
+        jLabel8.setBounds(10, 20, 150, 16);
+
+        jLabel9.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel9.setText("<html>\n5. Membawa jaket sesuai ketinggian <br>\nJaket atau pakaian hangat yang berbahan katun atau wol <br>\nadalah pilihan baik untuk mendaki gunung. Dan ingat jangan <br>\nmembawa jaket atau pakaian yang berbahan jeans karena \nbahan ini memang nampak kuat dan praktis, tetapi sulit sekali \nkering apabila basah.\n</html>");
+        pnl4.add(jLabel9);
+        jLabel9.setBounds(10, 40, 370, 130);
+
+        getContentPane().add(pnl4);
+        pnl4.setBounds(190, 90, 390, 330);
+
+        pnl3.setBackground(new java.awt.Color(25, 118, 210));
+        pnl3.setLayout(null);
+
+        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setText("Halaman 3");
+        pnl3.add(jLabel6);
+        jLabel6.setBounds(10, 20, 150, 16);
+
+        jLabel7.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel7.setText("<html>\n3. Bawa barang yang penting saja <br>\nMendaki gunung bukanlah piknik yang segala sesuatunya harus dibawa.\n Pilihlah barang bawaan dengan bijak. Selain hanya menambah beban tas,\nmembawa barang secara berlebihan juga bisa menghambat pendakian,\nmisalnya membawa tikar dengan gulungan besar. <br><br>\n\n4. Sleeping bag dan tenda <br>\nUntuk perlengkapan tidur, diperlukan tenda dan sleeping bag.<br>\nUntuk tenda, silakan bawa tenda sesuai dengan jumlah peserta <br>\nyang ikut. Membawa tenda terlalu besar juga terkadang bisa <br>\nmenghambat pendakian karena beratnya. <br>\nUntuk sleeping bag, silakan membawa sleeping bag yang sudah teruji kualitasnya.\nSelain itu, membawa matras juga sangat penting untuk keperluan tidur. Karena\nbiasanya tanah yang digunakan sebagai alas tetap terasa dingin meskipun sudah <br>\nmenggunakan sleeping bag.\n</html>");
+        pnl3.add(jLabel7);
+        jLabel7.setBounds(10, 40, 370, 280);
+
+        getContentPane().add(pnl3);
+        pnl3.setBounds(190, 90, 390, 330);
+
+        pnl2.setBackground(new java.awt.Color(25, 118, 210));
+        pnl2.setLayout(null);
+
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("Halaman 2");
+        pnl2.add(jLabel4);
+        jLabel4.setBounds(10, 20, 150, 16);
+
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setText("<html>\n1. Mengecek lokasi pendakian <br>\nSebelum memulai pendakian Anda disarankan mengecek dahulu lokasi pendakian dan <br>\nmengetahui jalur pendakian yang akan dilewati. Apakah keadaan gunung saat itu dalam kondisi <br>\nbaik untuk didaki atau kondisi gunung siaga satu dan tidak layak untuk didaki. Bertanya kepada <br>\nteman yang sudah hafal jalur pendakian juga penting dilakukan agar kita lebih siap melewati medan. <br> <br>\n\n2. Olahraga minimal sebulan sebelum pendakian <br>\nMendaki gunung membutuhkan fisik dan mental yang bugar. Oleh karena itu, <br>\ndisarankan untuk melakukan olahraga sebulan sebelum pendakian. Olahraga <br>\nyang biasa dilakukan biasanya jogging atau lari.\n</html>");
+        pnl2.add(jLabel5);
+        jLabel5.setBounds(10, 50, 360, 270);
+
+        getContentPane().add(pnl2);
+        pnl2.setBounds(190, 90, 390, 330);
+
+        pnl1.setBackground(new java.awt.Color(25, 118, 210));
+        pnl1.setLayout(null);
+
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Halaman 1");
+        pnl1.add(jLabel2);
+        jLabel2.setBounds(10, 20, 150, 16);
+
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("<html>\nMendaki gunung merupakan sebuah kegiatan luar ruangan yang menyenangkan, cukup <br>\nmenantang dan memacu adrenalin. Walaupun cukup menantang, tampaknya kegiatan <br>\nmendaki gunung cukup populer bagi kalangan masyarakat Indonesia. <br>\nNamun mendaki gunung sebaiknya tidak dianggap enteng. Perlu persiapan khusus yang <br> \nharus dilakukan para pendaki, terutama kondisi fisik dan mental yang bugar. Menganggap <br>\nremeh persiapan bisa berakibat fatal. Jika Anda ingin mendaki gunung, cobalah simak  <br>\ntips aman mendaki gunung berikut ini. <br>\n</html>");
+        pnl1.add(jLabel3);
+        jLabel3.setBounds(10, 50, 360, 230);
+
+        getContentPane().add(pnl1);
+        pnl1.setBounds(190, 90, 390, 330);
+
+        jPanel2.setBackground(new java.awt.Color(21, 101, 192));
+        jPanel2.setLayout(null);
+
+        jButton1.setText("Hal. 1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton1);
+        jButton1.setBounds(20, 20, 61, 32);
+
+        jButton2.setText("Hal. 2");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton2);
+        jButton2.setBounds(110, 20, 61, 32);
+
+        jButton3.setText("Hal. 3");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton3);
+        jButton3.setBounds(200, 20, 61, 32);
+
+        jButton4.setText("Hal. 4");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton4);
+        jButton4.setBounds(290, 20, 61, 32);
+
+        getContentPane().add(jPanel2);
+        jPanel2.setBounds(190, 0, 390, 70);
+
+        jPanel3.setBackground(new java.awt.Color(13, 71, 161));
+        jPanel3.setLayout(null);
+
+        jLabel1.setFont(new java.awt.Font("Dialog", 1, 30)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("<html>\n<center>\nTrip<br> Adventure\n</center>\n</html>");
+        jPanel3.add(jLabel1);
+        jLabel1.setBounds(0, 130, 180, 120);
+
+        getContentPane().add(jPanel3);
+        jPanel3.setBounds(0, 0, 170, 420);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        pnl1.setVisible(true);
+        pnl2.setVisible(false);
+        pnl3.setVisible(false);
+        pnl4.setVisible(false);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        pnl1.setVisible(false);
+        pnl2.setVisible(true);
+        pnl3.setVisible(false);
+        pnl4.setVisible(false);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        pnl1.setVisible(false);
+        pnl2.setVisible(false);
+        pnl3.setVisible(true);
+        pnl4.setVisible(false);
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        pnl1.setVisible(false);
+        pnl2.setVisible(false);
+        pnl3.setVisible(false);
+        pnl4.setVisible(true);
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+        // TODO add your handling code here:
+        pnl1.setVisible(true);
+        pnl2.setVisible(false);
+        pnl3.setVisible(false);
+        pnl4.setVisible(false);
+    }//GEN-LAST:event_formWindowOpened
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(FrmMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(FrmMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(FrmMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FrmMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FrmMain().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel pnl1;
+    private javax.swing.JPanel pnl2;
+    private javax.swing.JPanel pnl3;
+    private javax.swing.JPanel pnl4;
+    // End of variables declaration//GEN-END:variables
+}
